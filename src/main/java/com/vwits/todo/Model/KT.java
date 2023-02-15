78193fb48d0d09524189f240a87ade4c9a64fd68
Aadil <=>
2023-02-15 14:57:09 +0530

    Initial commit

---
+++ src/main/java/com/vwits/todo/Model/KT.java
@@ -0,0 +1,23 @@
+package com.vwits.todo.Model;
+
+interface calculate
+{
+    int add(int a,int b);
+
+
+}
+
+
+public class KT {
+    public static void main(String[] args) {
+        calculate add = Integer::sum;
+        System.out.println(add.add(3,2));
+
+    }
+}
+
+
+
+
+
+
